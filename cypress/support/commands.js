5c679bb5bd5340f609586e696310de7616d96f4f
Leonardo Setti <srvsecsub@protonmail.com>
2024-01-30 15:57:09 -0300

    :test_tube: Update Cypress project Tue Jan 30 03:57:09 PM -03 2024 from Linux archfidel 6.7.2-zen1-1-zen 2eabdab3d79df8343a62cf4a9975ccf047c939ad3a40010f49dd476a47116bb4  -

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,26 +1,39 @@
 Cypress.Commands.add("wName", () => {
   cy.get('input[name="firstName"]')
     .should("be.visible")
-    .type(Cypress.env("name"))
-    .should("have.value", Cypress.env("name"));
+    /*
+    .type(Cypress.env("name")) // use apenas localmente
+    .should("have.value", Cypress.env("name")); // use apenas localmente
+    */
+    .type("Carolyn")
+    .should("have.value", "Carolyn");
 });
 
 Cypress.Commands.add("wSurname", () => {
   cy.get('input[name="lastName"]')
     .should("be.visible")
-    .type(Cypress.env("surname"))
-    .should("have.value", Cypress.env("surname"));
+    /*
+    .type(Cypress.env("surname")) // use apenas localmente
+    .should("have.value", Cypress.env("surname")); // use apenas localmente
+    */
+    .type("Mackenzie")
+    .should("have.value", "Mackenzie");
 });
 
 Cypress.Commands.add("wEmail", () => {
   cy.get('input[id="email"]')
     .should("be.visible")
-    .type(Cypress.env("email"))
-    .should("have.value", Cypress.env("email"));
+    /*
+    .type(Cypress.env("email")) // use apenas localmente
+    .should("have.value", Cypress.env("email")) // use apenas localmente
+    */
+    .type("heather.blake@trust.me")
+    .should("have.value", "heather.blake@trust.me");
 });
 
 // Telefone não é um campo obrigatório desde que o check de telefone esteja vazio
 Cypress.Commands.add("wPhone", () => {
+  /* // use apenas localmente
   cy.get('input[id="phone-checkbox"]')
     .invoke("prop", "checked")
     .then((isChecked) => {
@@ -35,14 +48,34 @@ Cypress.Commands.add("wPhone", () => {
           .clear()
           .should("have.value", "");
       }
+  */
+
+  cy.get('input[id="phone-checkbox"]')
+    .invoke("prop", "checked")
+    .then((isChecked) => {
+      if (isChecked) {
+        cy.get('input[id="phone"]')
+          .should("be.visible")
+          .type("21997487965")
+          .should("have.value", "21997487965");
+      } else {
+        cy.get('input[id="phone"]')
+          .should("be.visible")
+          .clear()
+          .should("have.value", "");
+      }
     });
 });
 
 Cypress.Commands.add("wTxtarea", () => {
   cy.get('textarea[name="open-text-area"')
     .should("be.visible")
-    .type(Cypress.env("txtarea"), { delay: 0 })
-    .should("have.value", Cypress.env("txtarea"));
+    /*
+    .type(Cypress.env("txtarea"), { delay: 0 }) // use apenas localmente
+    .should("have.value", Cypress.env("txtarea")); // use apenas localmente
+    */
+    .type("Texto hard-coded para TextArea Element", { delay: 0 })
+    .should("have.value", "Texto hard-coded para TextArea Element");
 });
 //Envio do form:
 Cypress.Commands.add("hitSubmit", () => {
